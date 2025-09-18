b0b6d7cf5c5dbb9d5a532ea3a78b6650f14f436b
Kirill <kirill_alekseev-96@mail.ru>
2025-09-19 00:10:20 +0300

    Hashing Settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
-const path = require('node:path'); //обращение к протоколу
+const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js', // Точка входа
   output: {
-    filename: 'bundle.js', // Имя файла с результатом сборки
-    path: path.resolve(__dirname, 'build'), // Директория для файлов сборки(Путь к раб.)
+    filename: 'bundle.[contenthash].js', // Имя бандла
+    path: path.resolve(__dirname, 'build'), // Директория для файлов сборки
     clean: true, // Удаляем предыдущую сборку перед созданием новой
   },
-  devtool: 'source-map', // Генерируем карту исходного кода.
-  plugins: [
+  devtool: 'source-map', // Генерируем карту исходного кода
+  plugins: [ // Подключаем плагины
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
     new CopyPlugin({
-        patterns: [
-          {
-            from: 'public',
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
           },
-        ],
+        },
+      ],
     }),
   ],
   module: {
