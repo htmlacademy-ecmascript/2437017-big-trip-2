088abce8ab931c485804129184479d3500acf6b1
Kirill <kirill_alekseev-96@mail.ru>
2025-09-19 00:00:28 +0300

    Configuring the transpiler

--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,22 @@ module.exports = {
         ],
     }),
   ],
+  module: {
+    rules: [ // Добавляем лоадеры
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+    ],
+  },
 };
